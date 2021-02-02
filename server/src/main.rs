c4a8a9e3540c835ea55408c13e93d80de25cbd14
Lars Francke <lars.francke@gmail.com>
2021-02-02 09:48:24 +0100

    Fix build (follow latest operator-rs changes)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,11 @@
-use stackable_operator::error;
+use stackable_operator::{client, error};
 use stackable_zookeeper_crd::ZooKeeperCluster;
 use stackable_zookeeper_operator::create_controller;
 
 #[tokio::main]
 async fn main() -> Result<(), error::Error> {
     stackable_operator::initialize_logging("ZOOKEEPER_OPERATOR_LOG");
-    let client =
-        stackable_operator::create_client(Some("zookeeper.stackable.de".to_string())).await?;
+    let client = client::create_client(Some("zookeeper.stackable.de".to_string())).await?;
 
     stackable_operator::crd::ensure_crd_created::<ZooKeeperCluster>(client.clone()).await?;
 
