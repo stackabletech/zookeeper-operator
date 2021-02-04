8e192549266b770ef3ad20671792d271edcd4c2d
Lars Francke <lars.francke@gmail.com>
2021-02-04 17:08:57 +0100

    Fix import for best practices

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,5 @@
 use stackable_operator::{client, error};
 use stackable_zookeeper_crd::ZooKeeperCluster;
-use stackable_zookeeper_operator::create_controller;
 
 #[tokio::main]
 async fn main() -> Result<(), error::Error> {
@@ -9,6 +8,6 @@ async fn main() -> Result<(), error::Error> {
 
     stackable_operator::crd::ensure_crd_created::<ZooKeeperCluster>(client.clone()).await?;
 
-    create_controller(client).await;
+    stackable_zookeeper_operator::create_controller(client).await;
     Ok(())
 }
