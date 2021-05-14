05b01c47fc8c8376284c52ba3745921203a4bfe7
maltesander <malte.sander.it@gmail.com>
2021-05-14 09:46:01 +0200

    Merged main. Generalized example crd.

--- crd/src/lib.rs
+++ crd/src/lib.rs
@@ -85,10 +85,10 @@ impl ZookeeperVersion {
 
     pub fn package_name(&self) -> String {
         match self {
-            ZooKeeperVersion::v3_4_14 => {
+            ZookeeperVersion::v3_4_14 => {
                 format!("zookeeper-{}", self.to_string())
             }
-            ZooKeeperVersion::v3_5_8 => {
+            ZookeeperVersion::v3_5_8 => {
                 format!("apache-zookeeper-{}-bin", self.to_string())
             }
         }
@@ -141,14 +141,14 @@ mod tests {
     #[test]
     fn test_package_name() {
         assert_eq!(
-            ZooKeeperVersion::v3_4_14.package_name(),
-            format!("zookeeper-{}", ZooKeeperVersion::v3_4_14.to_string())
+            ZookeeperVersion::v3_4_14.package_name(),
+            format!("zookeeper-{}", ZookeeperVersion::v3_4_14.to_string())
         );
         assert_eq!(
-            ZooKeeperVersion::v3_5_8.package_name(),
+            ZookeeperVersion::v3_5_8.package_name(),
             format!(
                 "apache-zookeeper-{}-bin",
-                ZooKeeperVersion::v3_5_8.to_string()
+                ZookeeperVersion::v3_5_8.to_string()
             )
         );
     }
