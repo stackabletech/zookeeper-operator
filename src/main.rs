5deb34c0c6c34267c48f102d76951fe983a6607f
Teo Klestrup Röijezon <teo@nullable.se>
2021-10-14 16:26:33 +0200

    Import CRD definition from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod crd;
+
+use crd::ZookeeperCluster;
+use kube::CustomResourceExt;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(subcommand)]
+    cmd: Cmd,
+}
+
+#[derive(StructOpt)]
+enum Cmd {
+    /// Print CRD objects
+    Crd,
+}
+
+fn main() -> eyre::Result<()> {
+    let opts = Opts::from_args();
+    match opts.cmd {
+        Cmd::Crd => println!("{}", serde_yaml::to_string(&ZookeeperCluster::crd())?),
+    }
+    Ok(())
 }
