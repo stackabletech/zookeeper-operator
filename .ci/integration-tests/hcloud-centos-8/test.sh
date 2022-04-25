98e48145a033582a4b8e9ba2bf8d204e5c9b9e26
Nikolaus Winter <mail@nikolaus-winter.de>
2022-04-25 12:58:01 +0200

    minor fixes after linting

--- .ci/integration-tests/hcloud-centos-8/test.sh
+++ .ci/integration-tests/hcloud-centos-8/test.sh
@@ -1,4 +1,5 @@
-git clone -b $GIT_BRANCH https://github.com/stackabletech/zookeeper-operator.git
+#!/bin/bash
+git clone -b "$GIT_BRANCH" https://github.com/stackabletech/zookeeper-operator.git
 (cd zookeeper-operator/ && ./scripts/run_tests.sh)
 exit_code=$?
 ./operator-logs.sh zookeeper > /target/zookeeper-operator.log
