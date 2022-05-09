e6696cacf9767d959635746f602a47c8b6d9fc6e
Malte Sander <malte.sander.it@gmail.com>
2022-05-09 15:24:12 +0200

    improved test output

--- tests/templates/kuttl/smoke/test_tls.sh
+++ tests/templates/kuttl/smoke/test_tls.sh
@@ -1,18 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Usage: test_tls.sh namespace
 
 NAMESPACE=$1
+SERVER="test-zk-server-primary-1.test-zk-server-primary.${NAMESPACE}.svc.cluster.local:2282"
+
+echo "Start TLS testing..."
+############################################################################
 # test the plaintext unsecured connection
-COMMAND="/stackable/zookeeper/bin/zkCli.sh -server test-zk-server-primary-1.test-zk-server-primary.${NAMESPACE}.svc.cluster.local:2282 ls / >> file.log"
-$COMMAND
+############################################################################
+/stackable/zookeeper/bin/zkCli.sh -server ${SERVER} ls / &> /dev/null
 
 if [[ $? != 0 ]];
 then
-  echo "Could not establish unsecure connection..."
+  echo "[ERROR] Could not establish unsecure connection!"
   exit 1
 fi
-echo "Unsecure client connection established successfully!"
+echo "[SUCCESS] Unsecure client connection established!"
 
+############################################################################
 # we set the correct client tls credentials and expect to be able to connect
+############################################################################
 export CLIENT_STORE_SECRET=$(cat /stackable/rwconfig/zoo.cfg | grep "ssl.keyStore.password" | cut -d "=" -f2)
 export CLIENT_JVMFLAGS="
 -Dzookeeper.authProvider.x509=org.apache.zookeeper.server.auth.X509AuthenticationProvider
@@ -22,17 +29,18 @@ export CLIENT_JVMFLAGS="
 -Dzookeeper.ssl.keyStore.password=${CLIENT_STORE_SECRET}
 -Dzookeeper.ssl.trustStore.location=/stackable/tls/client/truststore.p12
 -Dzookeeper.ssl.trustStore.password=${CLIENT_STORE_SECRET}"
-
-$COMMAND
+/stackable/zookeeper/bin/zkCli.sh -server ${SERVER} ls / &> /dev/null
 
 if [[ $? != 0 ]];
 then
-  echo "Could not establish secure connection..."
+  echo "[ERROR] Could not establish secure connection using client certificates!"
   exit 1
 fi
-echo "Secure and authenticated client connection established successfully!"
+echo "[SUCCESS] Secure and authenticated client connection established!"
 
+############################################################################
 # We set the (wrong) quorum tls credentials and expect to fail (wrong certificate)
+############################################################################
 export QUORUM_STORE_SECRET=$(cat /stackable/rwconfig/zoo.cfg | grep "ssl.quorum.keyStore.password" | cut -d "=" -f2)
 export CLIENT_JVMFLAGS="
 -Dzookeeper.authProvider.x509=org.apache.zookeeper.server.auth.X509AuthenticationProvider
@@ -43,12 +51,14 @@ export CLIENT_JVMFLAGS="
 -Dzookeeper.ssl.trustStore.location=/stackable/tls/quorum/truststore.p12
 -Dzookeeper.ssl.trustStore.password=${QUORUM_STORE_SECRET}"
 
-$COMMAND
+/stackable/zookeeper/bin/zkCli.sh -server ${SERVER} ls / &> /dev/null
 
 if [[ $? == 0 ]];
 then
-  echo "Could establish secure connection with wrong certificates... this should not work!"
+  echo "[ERROR] Could establish secure connection with quorum certificates (should not be happening)!"
   exit 1
 fi
+echo "[SUCCESS] Could not establish secure connection with (wrong) quorum certificates!"
 
-echo "TLS test successful!"
+echo "All TLS tests successful!"
+exit 0
