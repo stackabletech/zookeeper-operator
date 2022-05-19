01fdf0b444f402abf9d8424f59970a6f75ad097a
Malte Sander <malte.sander.it@gmail.com>
2022-05-19 14:30:42 +0200

    attempt to fix linters

--- tests/templates/kuttl/smoke/test_heap.sh
+++ tests/templates/kuttl/smoke/test_heap.sh
@@ -5,12 +5,12 @@
 EXPECTED_HEAP=409
 
 # Check if ZK_SERVER_HEAP is set to the correct calculated value
-if [[ $ZK_SERVER_HEAP == $EXPECTED_HEAP ]]
+if [[ $ZK_SERVER_HEAP == "$EXPECTED_HEAP" ]]
 then
   echo "[SUCCESS] ZK_SERVER_HEAP set to $EXPECTED_HEAP"
 else
   echo "[ERROR] ZK_SERVER_HEAP not set or set with wrong value: $ZK_SERVER_HEAP"
-  exit -1
+  exit 1
 fi
 
 echo "[SUCCESS] All heap settings tests successful!"
