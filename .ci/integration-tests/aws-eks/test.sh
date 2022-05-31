da402ff097962a14605d6a5ed91b5ca6fdbc8aea
Nikolaus Winter <mail@nikolaus-winter.de>
2022-05-31 07:31:13 +0000

    Feature/test on managed k8s (#492)
    # Description
    
    added test definitions to run in managed K8s

---
+++ .ci/integration-tests/aws-eks/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone -b "$GIT_BRANCH" https://github.com/stackabletech/zookeeper-operator.git
+(cd zookeeper-operator/ && ./scripts/run_tests.sh --parallel 1)
+exit_code=$?
+./operator-logs.sh zookeeper > /target/zookeeper-operator.log
+exit $exit_code
