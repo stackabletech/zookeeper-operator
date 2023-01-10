abd959fa1ed87bb1689f89c98ea637eac29d17f2
Malte Sander <malte.sander.it@gmail.com>
2023-01-10 12:21:41 +0100

    removed deploy crd folder

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,9 +1,3 @@
-use stackable_operator::CustomResourceExt;
-use stackable_zookeeper_crd::{ZookeeperCluster, ZookeeperZnode};
-
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    ZookeeperCluster::write_yaml_schema("../../deploy/crd/zookeepercluster.crd.yaml").unwrap();
-    ZookeeperZnode::write_yaml_schema("../../deploy/crd/zookeeperznode.crd.yaml").unwrap();
 }
