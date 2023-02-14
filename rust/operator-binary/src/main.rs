81c1f7370c268eda86f95dc837b719b919f4d890
Sönke Liebau <soenke.liebau@stackable.tech>
2023-02-14 21:31:25 +0100

    Changes to become compatible with crate2nix

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -8,7 +8,7 @@ mod znode_controller;
 use crate::zk_controller::ZK_CONTROLLER_NAME;
 use crate::znode_controller::ZNODE_CONTROLLER_NAME;
 
-use clap::Parser;
+use clap::{crate_description, crate_version, Parser};
 use futures::StreamExt;
 use stackable_operator::{
     cli::{Command, ProductOperatorRun},
@@ -28,13 +28,14 @@ use std::sync::Arc;
 
 mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
+    pub const TARGET_PLATFORM: Option<&str> = option_env!("TARGET");
 }
 
 const APP_NAME: &str = "zookeeper";
 const OPERATOR_NAME: &str = "zookeeper.stackable.tech";
 
 #[derive(clap::Parser)]
-#[clap(about = built_info::PKG_DESCRIPTION, author = stackable_operator::cli::AUTHOR)]
+#[clap(about, author)]
 struct Opts {
     #[clap(subcommand)]
     cmd: Command,
@@ -59,10 +60,10 @@ async fn main() -> anyhow::Result<()> {
                 tracing_target,
             );
             stackable_operator::utils::print_startup_string(
-                built_info::PKG_DESCRIPTION,
-                built_info::PKG_VERSION,
+                crate_description!(),
+                crate_version!(),
                 built_info::GIT_VERSION,
-                built_info::TARGET,
+                built_info::TARGET_PLATFORM.unwrap_or("unknown target"),
                 built_info::BUILT_TIME_UTC,
                 built_info::RUSTC_VERSION,
             );
