b3249de376203185ddc439cb4e073f0d93806d41
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-02-15 16:49:02 +0100

    wip:group/catalog corrections

--- deploy/olm/23.1/olm_generate.sh
+++ deploy/olm/23.1/olm_generate.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # what should be the entry point for bundling/deploying - this script?
+#   - split into a) dockers and yamls b) installation
+#   - "make opm" (to update csv.yaml etc. and tee everything up)
 # How often do we need to create a bundle - every time the manifests change? i.e. every release
+#   - yes, every new version
 # generate-manifests.sh has been removed from templating, can maybe be re-purposed for openshift stuff..
 # manifests:
 # - roles.yaml --> same as the ClusterRole definition in helm/zookeeper-operator/templates/roles.yaml
@@ -9,6 +12,11 @@
 # - configmap.yaml --> embeds helm/zookeeper-operator/configs/properties.yaml
 # is the templates folder used at all? is it in danger of being removed? can we use for openshift?
 # how to parameterize the namespace?
+# the operator installs even when the source namespace doesn't match in subscription.yaml
+# the catalog-source deploys the operator w/o the subscription?
+# Tasks:
+#   - try crds.yaml instead of individual files
+#   -
 
 set -euo pipefail
 set -x
@@ -16,8 +24,10 @@ set -x
 main() {
   VERSION="$1";
 
+  # operator-specific, linked to an operator package
+  # done each and every time a new release is built
   opm alpha bundle generate --directory manifests \
-  --package zookeeper-operator --output-dir bundle \
+  --package stackable-operators --output-dir bundle \
   --channels stable --default stable
 
   docker build -t "docker.stackable.tech/sandbox/test/zookeeper-operator-bundle:${VERSION}" -f bundle.Dockerfile .
@@ -25,44 +35,52 @@ main() {
 
   opm alpha bundle validate --tag "docker.stackable.tech/sandbox/test/zookeeper-operator-bundle:${VERSION}" --image-builder docker
 
+
   echo "Creating Dockerfile..."
   if [ -d "catalog" ]; then
     rm -rf catalog
   fi
 
+  # catalog (just creates dockerfile with copy command): this is not done for each release
   mkdir -p catalog
   rm -f catalog.Dockerfile
   opm generate dockerfile catalog
 
-  echo "Initiating operator..."
-  opm init zookeeper-operator \
+  # operator package: create/init
+  echo "Initiating package..."
+  opm init stackable-operators \
       --default-channel=preview \
       --description=./README.md \
-      --output yaml > catalog/operator.yaml
+      --output yaml > catalog/stackable-operators.yaml
 
-  # TODO this is ugly. Try cat?
+  # operator added to operator package: iterate over operator list
   {
     echo "---"
     echo "schema: olm.channel"
-    echo "package: zookeeper-operator"
+    echo "package: stackable-operators"
     echo "name: preview"
     echo "entries: "
     echo "- name: zookeeper-operator.v${VERSION}"
-  } >> catalog/operator.yaml
+  } >> catalog/stackable-operators.yaml
 
+  # iterate over operator(-bundle) list
   echo "Rendering operator..."
-  opm render "docker.stackable.tech/sandbox/test/zookeeper-operator-bundle:${VERSION}" --output=yaml >> catalog/operator.yaml
+  opm render "docker.stackable.tech/sandbox/test/zookeeper-operator-bundle:${VERSION}" --output=yaml >> catalog/stackable-operators.yaml
 
   echo "Validating catalog..."
   opm validate catalog
 
-  docker build . -f catalog.Dockerfile -t "docker.stackable.tech/sandbox/test/zookeeper-operator-catalog:${VERSION}"
-  docker push "docker.stackable.tech/sandbox/test/zookeeper-operator-catalog:${VERSION}"
+  # build catalog for all operators
+  docker build . -f catalog.Dockerfile -t "docker.stackable.tech/sandbox/test/stackable-operators-catalog:${VERSION}"
+  docker push "docker.stackable.tech/sandbox/test/stackable-operators-catalog:${VERSION}"
 
+  # install catalog/group for all operators
   echo "Installing operator..."
-  #kubectl apply -f catalog-source.yaml
-  #kubectl apply -f operator-group.yaml
-  #kubectl apply -f subscription.yaml
+  kubectl apply -f catalog-source.yaml
+  kubectl apply -f operator-group.yaml
+
+  # iterate over operator list to deploy
+  kubectl apply -f zookeeper-subscription.yaml
 
   echo "Deployment successful!"
 }
