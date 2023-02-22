66afec79ffd2687fd4b7f3d4149064a3cda3744f
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-02-22 14:58:03 +0100

    Regenerate charts, docs and remove default pod affinity.

--- rust/crd/src/affinity.rs
+++ rust/crd/src/affinity.rs
@@ -1,24 +1,16 @@
 use stackable_operator::{
-    commons::affinity::{
-        affinity_between_cluster_pods, affinity_between_role_pods, StackableAffinityFragment,
-    },
-    k8s_openapi::api::core::v1::{PodAffinity, PodAntiAffinity},
+    commons::affinity::{affinity_between_role_pods, StackableAffinityFragment},
+    k8s_openapi::api::core::v1::PodAntiAffinity,
 };
 
 use crate::{ZookeeperRole, APP_NAME};
 
 pub fn get_affinity(cluster_name: &str, role: &ZookeeperRole) -> StackableAffinityFragment {
-    let affinity_between_cluster_pods = affinity_between_cluster_pods(APP_NAME, cluster_name, 20);
     let affinity_between_role_pods =
         affinity_between_role_pods(APP_NAME, cluster_name, &role.to_string(), 70);
 
     StackableAffinityFragment {
-        pod_affinity: Some(PodAffinity {
-            preferred_during_scheduling_ignored_during_execution: Some(vec![
-                affinity_between_cluster_pods,
-            ]),
-            required_during_scheduling_ignored_during_execution: None,
-        }),
+        pod_affinity: None,
         pod_anti_affinity: Some(PodAntiAffinity {
             preferred_during_scheduling_ignored_during_execution: Some(vec![
                 affinity_between_role_pods,
