0410a1007a2ab37624d93f0493e352fde2f0f4b0
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-06-08 15:30:41 +0200

    Raise res requests for the init container again

--- rust/operator-binary/src/zk_controller.rs
+++ rust/operator-binary/src/zk_controller.rs
@@ -675,6 +675,14 @@ fn build_server_rolegroup_statefulset(
         .add_volume_mount("config", STACKABLE_CONFIG_DIR)
         .add_volume_mount("rwconfig", STACKABLE_RW_CONFIG_DIR)
         .add_volume_mount("log", STACKABLE_LOG_DIR)
+        .with_resources(
+            ResourceRequirementsBuilder::new()
+                .with_cpu_limit("500m")
+                .with_cpu_request("100m")
+                .with_memory_limit("40Mi")
+                .with_memory_request("8Mi")
+                .build(),
+        )
         .build();
 
     let container_zk = cb_zookeeper
