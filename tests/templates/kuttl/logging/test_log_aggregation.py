1e9209e8d3fef31095bf4df7da70fdf1a374cb54
Malte Sander <malte.sander.it@gmail.com>
2023-08-02 09:48:07 +0200

    fix linter

--- tests/templates/kuttl/logging/test_log_aggregation.py
+++ tests/templates/kuttl/logging/test_log_aggregation.py
@@ -35,12 +35,12 @@ def check_sent_events():
 
         if componentId == 'filteredInvalidEvents':
             assert sentEvents is None or \
-                sentEvents['sentEventsTotal'] == 0, \
-                'Invalid log events were sent.'
+                   sentEvents['sentEventsTotal'] == 0, \
+                   'Invalid log events were sent.'
         else:
             assert sentEvents is not None and \
-                sentEvents['sentEventsTotal'] > 0, \
-                f'No events were sent in "{componentId}".'
+                   sentEvents['sentEventsTotal'] > 0, \
+                   f'No events were sent in "{componentId}".'
 
 
 if __name__ == '__main__':
