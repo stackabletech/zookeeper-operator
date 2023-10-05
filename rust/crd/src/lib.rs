58152067d9875ede7d27eb4dc2c0c8d459d881f0
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-10-05 14:01:07 +0200

    Change PDBs to maxUnavailable: 1

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -525,15 +525,6 @@ impl ZookeeperCluster {
         }
     }
 
-    pub fn num_servers(&self) -> u16 {
-        self.spec
-            .servers
-            .iter()
-            .flat_map(|s| s.role_groups.values())
-            .map(|rg| rg.replicas.unwrap_or(1))
-            .sum()
-    }
-
     /// List all pods expected to form the cluster
     ///
     /// We try to predict the pods here rather than looking at the current cluster state in order to
