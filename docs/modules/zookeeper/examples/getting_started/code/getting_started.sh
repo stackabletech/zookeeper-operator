e86d13eea6ae80cc1394c956c0d2e19d2f9f8171
Techassi <git@techassi.dev>
2026-06-01 16:32:14 +0200

    chore(getting_started): Bump ZooKeeper version to 3.9.5

--- docs/modules/zookeeper/examples/getting_started/code/getting_started.sh
+++ docs/modules/zookeeper/examples/getting_started/code/getting_started.sh
@@ -70,7 +70,7 @@ zkCli_ls() {
 # tag::zkcli-ls[]
 kubectl run my-pod \
   --stdin --tty --quiet --restart=Never \
-  --image oci.stackable.tech/sdp/zookeeper:3.9.4-stackable0.0.0-dev -- \
+  --image oci.stackable.tech/sdp/zookeeper:3.9.5-stackable0.0.0-dev -- \
   bin/zkCli.sh -server simple-zk-server:2282 ls / > /dev/null && \
   kubectl logs my-pod && \
   kubectl delete pods my-pod
