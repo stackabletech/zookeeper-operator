8e29e8b6d9c64db2c1112d738c0f34520ff78d1d
Andrew Kenworthy <andrew.kenworthy@stackable.tech>
2026-07-27 15:39:50 +0200

    use the role parameter via its From impl in build_pdb

--- rust/operator-binary/src/zk_controller/build/resource/pdb.rs
+++ rust/operator-binary/src/zk_controller/build/resource/pdb.rs
@@ -24,7 +24,7 @@ pub fn build_pdb(
     let pdb = pod_disruption_budget_builder_with_role(
         cluster,
         &product_name(),
-        &ZookeeperRole::Server.into(),
+        &role.into(),
         &operator_name(),
         &controller_name(),
     )
